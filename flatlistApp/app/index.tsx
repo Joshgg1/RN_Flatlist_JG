0f8e15d0014805f25e3aed2c83cd674cf06f0f9f
Joshua George <george3191@students.d211.org>
2025-01-29 11:05:22 -0600

    dio

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -7,8 +7,30 @@ import {
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
-// woo hoo! We have a flatlist!
+import { useState } from "react";
+
+type dataType = {
+  id: string; // UID(unique identifier) for each element in list
+  title: string; // what you want to display
+}
+
 export default function Index() {
+  const DATA: dataType[] = [
+    { id: "1", title: "First Item" },
+    { id: "2", title: "Second Item" },
+    { id: "3", title: "Third Item" },
+    { id: "4", title: "Fourth Item" },
+  ]
+
+  const [selectedId, setSelectedId] = useState<string>("");
+
+  // create simple function to call when item is selected
+  // pass a parameter of the item selected
+  const selectedList = (item: dataType) => {
+    console.log("Click on item: ", item.title);
+    setSelectedId(item.id);
+  }
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +38,21 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go, yay!</Text>
+          <FlatList
+            data={DATA}
+            keyExtractor={(item: dataType) => item.id}
+            renderItem = {({ item }) => (
+              <TouchableOpacity onPress={() => selectedList(item)}>
+                <View style={[styles.titleContainer,{
+                  backgroundColor: selectedId === item.id ? colors.primary : colors.secondary
+                }]}>
+                  <Text style={[defaultStyles.title,{
+                    color: selectedId === item.id ? colors.text.light : colors.text.dark
+                  }]}>{item.title}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
       </View>
     </View>
